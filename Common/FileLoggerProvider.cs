58da7c982e5e94f413309a1f5eddc2ccbfc9599c
VladislavStarovoitov <vladislav.starovoitov94@gmail.com>
2018-07-03 00:38:55 +0300

    Init

---
+++ Common/FileLoggerProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Common
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string path)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Web/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ILoggerFactory loggerFactory)
+        {
+            ILogger logger = loggerFactory.CreateLogger("logs/memoryLog.txt");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            await _next.Invoke(context);
+            watch.Stop();
+            logger.LogInformation($"Request time: {watch.ElapsedMilliseconds}ms.");
+        }
+    }
+}
